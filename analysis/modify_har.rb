32bbebc05877e29645a48f5ec9950d44a48bf44b
colin-scott <cs@cs.berkeley.edu>
2014-11-03 19:56:14 -0800

    Remove debug prints

--- analysis/modify_har.rb
+++ analysis/modify_har.rb
@@ -33,8 +33,6 @@ def assume_perfect_cache!(har)
       timings['wait'] = 0
       timings['receive'] = 0
       timings['ssl'] = -1
-    else
-      puts "Not cacheable! Note rewriting #{entry.inspect}"
     end
   end
 end
@@ -45,12 +43,10 @@ def is_cacheable(response)
   response['headers'].each do |header|
     if header['name'] =~ /cache-control/i
       puts "Warning: two CC headers. Previous: #{cc_header} Current: #{header['name']}" unless cc_header.empty?
-      puts "cc_header! #{header.inspect}"
       cc_header = header['value']
     end
     if header['name'] =~ /expires/i
       puts "Warning: two CC headers. Previous: #{expires_header} Current: #{header['name']}" unless expires_header.empty?
-      puts "expires_header! #{header.inspect}"
       expires_header = header['value']
     end
   end
@@ -60,7 +56,6 @@ def is_cacheable(response)
   #   AND LENGTH(resp_expires) = 0
   #   THEN "undefined"
   if cc_header.empty? and expires_header.empty?
-    puts "undefined"
     return true
   end
 
@@ -73,12 +68,10 @@ def is_cacheable(response)
       cc_header =~ /no-cache/i or
       cc_header =~ /max-age=0/i or
       expires_header =~ /-1/)
-    puts "Not Cacheable"
     return false
   end
 
   # ELSE "cacheable"
-  puts "Cacheable"
   return true
 end
 
