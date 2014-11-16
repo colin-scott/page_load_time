8ae2616bfffd2926a2c7336d7b35b6b495a77c52
colin-scott <cs@cs.berkeley.edu>
2014-11-16 15:57:04 -0800

    Don't remove non .har files

--- data/remove_all_invalid_har_files.rb
+++ data/remove_all_invalid_har_files.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
 
+# For your own safety, file *must* end in .har.
+
 require_relative '../analysis/har_util.rb'
 
 ARGV.each do |file|
  begin
-   parse_har_file file
-   puts "#{file} is valid"
+   if file.end_with? ".har"
+     parse_har_file file
+     puts "#{file} is valid"
+   else
+     puts "#{file} does not have .har extension"
+   end
  rescue
    puts "Deleting #{file}"
    File.delete(file)
