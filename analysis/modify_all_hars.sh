94a40c54a016a6271c5dcb3adf7ef70e0fc4f7f3
colin-scott <cs@cs.berkeley.edu>
2014-11-16 15:58:01 -0800

    Don't create .pc.pc's

--- analysis/modify_all_hars.sh
+++ analysis/modify_all_hars.sh
@@ -3,5 +3,7 @@
 # TODO(cs): handle case where there are too many files in a directory to fit in $@
 
 for i in $@; do
-  ./modify_har.rb $i ${i/.har/.pc.har}
+  if [ ! -f ${i/.har/.pc.har} ]; then
+    ./modify_har.rb $i ${i/.har/.pc.har}
+  fi
 done
