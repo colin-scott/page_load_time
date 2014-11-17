fde0d6966affe32009ff1b99285f8154392956cb
colin-scott <cs@cs.berkeley.edu>
2014-11-16 18:02:19 -0800

    Don't modify twice

--- analysis/modify_all_hars.sh
+++ analysis/modify_all_hars.sh
@@ -3,7 +3,9 @@
 # TODO(cs): handle case where there are too many files in a directory to fit in $@
 
 for i in $@; do
-  if [ ! -f ${i/.har/.pc.har} ]; then
+  if [[ ${i/.har/.pc.har} != *pc.pc.* ]]; then
     ./modify_har.rb $i ${i/.har/.pc.har}
+  else
+    echo "Skipping $i. Already modified."
   fi
 done
