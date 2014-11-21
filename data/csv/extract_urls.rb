8bb17063c96a54c988340db68fdbfd8ab9d524aa
colin-scott <cs@cs.berkeley.edu>
2014-11-20 21:12:32 -0800

    Add working script for fetching URLs

--- data/csv/extract_urls.rb
+++ data/csv/extract_urls.rb
@@ -2,7 +2,9 @@
 
 require "csv"
 
+filter_adult_sites = true
+
 text = File.read(ARGV.shift).gsub(/(?<!\\)\\"/,'""')
 CSV.parse text do |row|
-  puts row[7]
+  puts row[7] unless filter_adult_sites and row[64] == 1
 end
---
+++ data/fetch_urls_sequentially.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import signal
+import sys
+
+import telemetry_web_page_replay as wpr
+from telemetry_util import FileSafeName
+
+if os.geteuid() != 0:
+  print "Must run as root"
+  sys.exit(1)
+
+def fetch(url):
+  print "Fetching ", url
+  filename = FileSafeName(url)
+  wpr_output = "wpr/" + filename + ".wpr"
+  phantomjs_err = "wpr/" + filename + ".err"
+  har_output = "har/" + filename + ".har"
+  with wpr.ReplayServer(wpr_output, replay_options=["--record"]):
+    subprocess.Popen("phantomjs --disk-cache=false netsniff.js '%s'>'%s' 2>'%s'" %
+                     (url, har_output, phantomjs_err), shell=True)
+
+with open("target_urls") as f:
+  for url in f:
+    url = url.strip()
+    fetch(url)
