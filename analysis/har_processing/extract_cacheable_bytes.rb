a4ece6a99155e235341a3a6c08c8b304c8a6a338
colin-scott <cs@cs.berkeley.edu>
2014-11-21 14:42:41 -0800

    Add script for extracting cacheable bytes from HAR files

---
+++ analysis/har_processing/extract_cacheable_bytes.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require_relative 'har_util.rb'
+
+def extract_cacheable_bytes(har)
+  total_cacheable_bytes = 0
+  # WPT only tracks bytes in before Document Complete time. This tracks all
+  # bytes in.
+  total_bytes_in = 0
+  har['log']['entries'].each do |entry|
+    ['headersSize', 'bodySize'].each do |part|
+      if entry['response'][part].nil?
+        puts "Warning: #{part} nil?. Entry: #{entry['response'].inspect}"
+      elsif entry['response'][part] != -1
+        total_bytes_in += entry['response'][part]
+        total_cacheable_bytes += entry['response'][part] if is_cacheable(entry['response'])
+      end
+    end
+  end
+
+  page = har['log']['pages'][0]
+  url = page['id']
+  cacheable_bytes_fraction = total_cacheable_bytes * 1.0 / total_bytes_in
+  puts "#{url} #{cacheable_bytes_fraction} #{total_cacheable_bytes} #{total_bytes_in}"
+end
+
+if __FILE__ == $0
+  if ARGV.length != 1
+    puts "#{__FILE__} <directory containing HARs>"
+    exit 1
+  end
+
+  Dir.glob(ARGV.shift + "/*.har").each do |file|
+    har = parse_har_file(file)
+    extract_cacheable_bytes(har)
+  end
+end
