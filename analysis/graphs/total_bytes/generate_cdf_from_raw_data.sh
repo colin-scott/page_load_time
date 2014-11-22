8272f781ec8483bfc2c40b35e3079314c7aa4ca3
colin-scott <cs@cs.berkeley.edu>
2014-11-21 16:18:28 -0800

    Script for generating CDF of total bytes

---
+++ analysis/graphs/total_bytes/generate_cdf_from_raw_data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "Usage: $0 <output of extract_cacheable_bytes.rb>"
+  exit 1
+fi
+
+# Format:
+#"#{url} #{cacheable_bytes_fraction} #{total_cacheable_bytes} #{total_bytes_in}"
+
+cut -d ' ' -f 3 $1 | sort -n > total_bytes.cdf
+~/Scripts/cdf/compile_ccdf.pl total_bytes.cdf > buf && mv buf total_bytes.cdf
+gnuplot total_bytes.gpi
+open *pdf
