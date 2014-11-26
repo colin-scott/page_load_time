bfb3aa24325278bb4409748afb4af4b8d65268bf
colin-scott <cs@cs.berkeley.edu>
2014-11-26 11:51:06 -0800

    Add .err to retrieved data

--- replay_vms/retrieve_data.sh
+++ replay_vms/retrieve_data.sh
@@ -4,5 +4,6 @@ pslurp -h hosts -l vagrant -t 0 -A -L ../data/replay --recursive /home/vagrant/p
 cd ../data/replay
 for F in 10.9.1.*; do
   find $F/replay -name "*har" | xargs -I '{}' mv '{}' .
+  find $F/replay -name "*err" | xargs -I '{}' mv '{}' .
   rm -rf $F
 done
