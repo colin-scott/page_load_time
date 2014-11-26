a429ce3d399e65876e7ca566b3c09a6be52dc5bd
colin-scott <cs@cs.berkeley.edu>
2014-11-26 01:45:21 -0800

    Changes that were neglected to be checked in

--- data/telemetry_util.py
+++ data/telemetry_util.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Copyright 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -183,3 +184,6 @@ def IsRunningOnCrosDevice():
       if res.count('CHROMEOS_RELEASE_NAME'):
         return True
   return False
+
+if __name__ == '__main__':
+  print FileSafeName(sys.argv[1])
