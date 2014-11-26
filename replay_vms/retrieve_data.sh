34dd7ad7ae86e6e1be36235879f2afb7988d798d
colin-scott <cs@cs.berkeley.edu>
2014-11-25 18:31:41 -0800

    account for too many args

--- replay_vms/retrieve_data.sh
+++ replay_vms/retrieve_data.sh
@@ -3,6 +3,6 @@
 pslurp -h hosts -l vagrant -t 0 -A -L ../data/replay --recursive /home/vagrant/page_load_time/data/replay/ replay
 cd ../data/replay
 for F in 10.9.1.*; do
-  mv $F/replay/* .
+  find $F/replay -name "*har" | xargs -I '{}' mv '{}' .
   rm -rf $F
 done
