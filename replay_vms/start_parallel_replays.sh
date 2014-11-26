a91d0bb28e62ba37356a0d79866a00ea7cfa381f
root <root@c140.Millennium.Berkeley.EDU>
2014-11-25 16:59:59 -0800

    Add script for running replays in parallel

--- replay_vms/start_parallel_replays.sh
+++ replay_vms/start_parallel_replays.sh
@@ -1,8 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# ssh to each machine, run
-# cd data
-# sudo ./replay_urls_sequentially.py ./wpr/
-for F in slave2 slave3 slave4 slave5; do
-  vagrant ssh $F -c "cd data; sudo ./replay_urls_sequentially.py ./wpr"
-done
+pssh -h hosts -l vagrant -t 0 -A -i "cd page_load_time/data; sudo ./replay_urls_sequentially.py ./wpr"
