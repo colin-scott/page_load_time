59911812bc094dec804f3c934acb4458107adf0a
colin-scott <cs@cs.berkeley.edu>
2014-11-25 17:58:31 -0800

    Accomodate older versions of the s/w

--- replay_vms/retrieve_data.sh
+++ replay_vms/retrieve_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pslurp -h hosts -l vagrant -t 0 -A -L ../data/replay --recursive page_load_time/data/replay/ replay
+pslurp -h hosts -l vagrant -t 0 -A -L ../data/replay --recursive /home/vagrant/page_load_time/data/replay/ replay
 cd ../data/replay
 for F in 10.9.1.*; do
   mv $F/replay/* .
