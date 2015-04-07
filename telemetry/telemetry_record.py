df1275416bd17f5091055b61cb632c7ccf9562be
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-04-07 02:46:12 -0700

    Urls to single pass benchmark; Unrefined

--- telemetry/telemetry_record.py
+++ telemetry/telemetry_record.py
@@ -62,9 +62,22 @@ def write_page_sets(urls):
         template = ''.join(f.readlines())
         assert template, 'Failed to read from page set template'
     for i in range(len(urls)):
-        test_name = 'test{0}'.format(i)
+        test_name = 'url{0}'.format(i)
         with open('{0}{1}.py'.format(page_set_path, test_name), 'wb') as f:
             f.write(template.format(test_name, urls[i]))
+
+def store_url_mapping(url_list):
+    """Stores a mapping from index to url to be used later.
+
+    Saved to data/url_mapping.db: {index: url,...}
+    """
+    url_mapping = {}
+    for i in range(len(url_list)):
+        url_mapping[i] = url_list[i]
+    try:
+        pickle.dump(url_mapping, open('data/url_mapping.db', 'wb'))
+    except IOError:
+        raise IOError('Failed to write url mapping to data/url_mapping.db')
 
 def move_wpr_files(name_schema):
     """Moves wpr files from within CHROMIUM_SRC to local data directory
@@ -73,12 +86,12 @@ def move_wpr_files(name_schema):
     """
     remote_data_path = os.path.join(CHROMIUM_SRC, 'tools/perf/page_sets/data/',
             name_schema)
-    local_path = os.path.join(os.getcwd(), 'data/')
+    local_path = os.path.join(os.getcwd(), 'data/wpr_source')
     p = Popen('mv {0} {1}'.format(remote_data_path, local_path), shell=True)
 
 def modify_wpr():
-    """Sets cacheable objects delay time to 0, creates  pc.wpr files"""
-    wpr_directory = os.path.join(os.getcwd(), 'data/')
+    """Sets cacheable objects delay time to 0, creates pc.wpr files"""
+    wpr_directory = os.path.join(os.getcwd(), 'data/wpr_source')
     wpr_path = os.path.join(CHROMIUM_SRC,
             'third_party/webpagereplay/modify_wpr_delays.py')
     p = Popen('python {0} {1}'.format(wpr_path, wpr_directory), shell=True)
@@ -96,6 +109,69 @@ def failed_url(url, output):
     failed_set[url] = output
     pickle.dump(failed_set, open('failed_urls', 'a'))
 
+def copy_wpr_to_benchmark():
+    """Moves wpr and .pc.wpr (all) files from local data/wpr_source to chromium
+
+    located: /src/tools/perf/page_sets/data/
+    """
+    local_path = os.path.join(os.getcwd(), 'data/wpr_source/*')
+    remote_data_path = os.path.join(CHROMIUM_SRC, 'tools/perf/page_sets/data/')
+    p = Popen('cp {0} {1}'.format(local_path, remote_data_path), shell=True)
+
+def prepare_benchmark(trial_number, num_urls):
+    """Simply checks to make sure the env is prepped before benchmarking
+
+    1. Ensures that ~/page_load_time/telemetry/temp/benchmark_results.db exists
+    2. Writes benchmarks to ~/src/tools/perf/benchmarks/page_cycler.py if they
+       do not already exist
+    """
+    # Ensure benchmark_results.db exists
+    telemetry_record_path = \
+    '/home/jamshed/page_load_time/telemetry/temp/benchmark_results.db'
+    if not os.path.isfile(telemetry_record_path):
+        # Create file
+        tmp = {trial_number: {}}
+        pickle.dump(tmp, open(telemetry_record_path, 'wb'))
+    try:
+        curr_pickle = pickle.load(open(telemetry_record_path, 'rb'))
+    except IOError:
+        # Raise exception or rewrite benchmark_results.db?
+        tmp = {trial_number: {}}
+        pickle.dump(tmp, open(telemetry_record_path, 'wb'))
+
+    # Write benchmarks to page_cycler.py
+    page_cycler_path = '/home/jamshed/src/tools/perf/benchmarks/page_cycler.py'
+    benchmark_template = ("@benchmark.Enabled('android')\n"
+                          "class PageCyclerUrl{0}(_PageCycler):\n"
+                          "    page_set = page_sets.url{0}PageSet\n\n"
+        )
+
+    with open(page_cycler_path, 'r+b') as f:
+        line = f.readline()
+        already_written = False
+        while (line):
+            if '### END OF URLS ###' in line:
+                already_written = True
+                break
+            line = f.readline()
+        if not already_written:
+            # Need to write benchmarks to page_cycler.py, for now, just append
+            f.write('\n\n')
+            for i in range(num_urls):
+                f.write(benchmark_template.format(i))
+            f.write('### END OF URLS ###')
+
+def run_benchmarks(num_urls):
+    """Runs the page_cycler benchmark for each url
+
+    Dumps data /temp/benchmark_results.db
+    """
+    path.append('/home/jamshed/src/tools/perf/')
+    cmd = ('sudo /home/jamshed/src/tools/perf/run_benchmark '
+           'page_cycler.PageCyclerUrl{0}')
+    for i in range(num_urls):
+        p = Popen(cmd.format(i), shell=True)
+        p.wait()  # We can only load 1 url on Chrome at a time
 
 def __main__():
 
@@ -107,15 +183,25 @@ def __main__():
     urls = get_urls(target_url_path)
     write_page_sets(urls)
     # Record each one, measure plt
+    store_url_mapping(urls)
     for i in range(len(urls)):
-        test_name = 'test{0}'.format(i)
+        test_name = 'url{0}'.format(i)
         record('{0}_page_set'.format(test_name), urls[i])
     # Move files here
-    move_wpr_files('test*_page_set_*.wpr')
+    move_wpr_files('url*_page_set_*.wpr')
     # Modify wpr
     modify_wpr()
-    # Re-record, measure plt
+    # Move files to benchmark location
+    copy_wpr_to_benchmark()
+    # Prepare benchmark
+    TRIAL_NUMBER = 1
+    prepare_benchmark(TRIAL_NUMBER, len(urls))
+    # Run benchmarks for each page set (url), N times, aggregating data
+    run_benchmarks(len(urls))
+    #aggregate_benchmark_data() # THEN RUN MORE TIMES
+    # Convert benchmark results to har format
     # Analysis
+    # Reset wpr files and benchmarks
 
 if __name__ == '__main__':
     __main__()
