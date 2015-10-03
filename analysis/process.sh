0a19de725b7783593ca225b283739d72decd8447
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-10-03 14:40:59 -0700

    Removed some echoing

--- analysis/process.sh
+++ analysis/process.sh
@@ -2,7 +2,7 @@
 cut -d ' ' -f 2 ../data/filtered_stats/valids.txt > valid_originals.txt
 echo "extracting cacheable bytes"
 ./har_processing/extract_cacheable_bytes.rb valid_originals.txt | sort | uniq > cacheable_bytes.dat
-echo "extracting plt"
+echo "Extracting plt"
 ./har_processing/extract_plt.rb valid_originals.txt > replay_plts.dat
 egrep '.pc.har ' replay_plts.dat > perfect_cache_replay_plts.dat
 egrep -v '.pc.har ' replay_plts.dat > unmodified_replay_plts.dat
@@ -14,12 +14,9 @@ egrep -v '.pc.har ' replay_plts.dat > unmodified_replay_plts.dat
 echo "Generating CDFs"
 cd ./graphs/cacheable_bytes/; ./generate_cdf_from_raw_data.sh ../../cacheable_bytes.dat
 cd ../../
-echo "here1"
 cd ./graphs/plts/; ./generate_cdf_from_raw_data.sh ../../unmodified_replay_plts.dat ../../perfect_cache_replay_plts.dat
 cd ../../
-echo "here2"
 cd ./graphs/percent_plt_reduction/; ./generate_cdf_from_raw_data.sh ../../median_plt_reduction.dat
 cd ../../
-echo "here3"
 cd ./graphs/ratio_bytes_to_reduction/; ./generate_cdf_from_raw_data.sh ../../ratio.dat
 cd ../../
