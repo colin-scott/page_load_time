f3c4cefb8b8baaa8f69b766efe50b6008098793a
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-12-22 22:00:28 -0800

    Optimized timeline to status codes for any file

--- telemetry/sanity_checks/get_status_codes_from_timeline.py
+++ telemetry/sanity_checks/get_status_codes_from_timeline.py
@@ -4,10 +4,24 @@ Requires proper json formatting
 """
 from collections import Counter
 from re import search
+from subprocess import call
+import sys
 
 def __main__():
+
+    args = sys.argv
+
+    if len(args) != 2:
+        raise Exception("Must provide json file")
+
+    return_code = \
+    call('cat {0} | python -m json.tool > /tmp/tmp.json'.format(args[1]), shell=True)
+
+    if return_code != 0:
+        raise Exception('Unable to pretty print ~/events.json')
+
     content = []
-    with open('/home/jamshed/events.json', 'rb') as f:
+    with open('/tmp/tmp.json', 'rb') as f:
         content = f.readlines()
 
     if content == [] or len(content) < 10:
@@ -21,10 +35,17 @@ def __main__():
         misc = content[i+1]
         status_code = content[i+2]
         if 'statusCode' in status_code:
-            code =  search('([0-9]+)', status_code).group(0)
-            object_type =  search('([a-zA-Z]+\/[a-zA-Z]+)', label).group(0)
-            status_code_dict[code] += 1
-            type_dict[object_type] += 1
+            try:
+                code =  search('([0-9]+)', status_code)
+                if code is not None:
+                    code = code.group(0)
+                object_type =  search('([a-zA-Z]+\/[a-zA-Z]+)', label)
+                if object_type is not None:
+                    object_type = object_type.group(0)
+                status_code_dict[code] += 1
+                type_dict[object_type] += 1
+            except:
+                import ipdb; ipdb.set_trace()
 
     print status_code_dict
     print type_dict
